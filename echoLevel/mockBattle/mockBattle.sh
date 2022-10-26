398c1e05fdafa482088ec028f1665fc76960d6ec
Gabriel Lopez <gabriel.andres.lopez03@gmail.com>
2022-10-25 21:17:43 -0400

    added starting script and training directory

---
+++ echoLevel/mockBattle/mockBattle.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "User Accepted"
+echo "Password Accepted"
+echo "-----------ENTERING SIMULATION----------"
+echo "Welcome user"
+echo "Start training by going to the training directory"
