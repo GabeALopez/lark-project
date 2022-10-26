8342e1a14e68ae8f5fba30597478c12dadf2154c
Gabriel Lopez <gabriel.andres.lopez03@gmail.com>
2022-10-25 22:25:59 -0400

    edited challenges with string formatting

--- echoLevel/mockBattle/training/challenges.sh
+++ echoLevel/mockBattle/training/challenges.sh
@@ -2,17 +2,25 @@
 
 case $1 in
 1)
-        echo "Now try appending into the same file with echo"
+	echo "---------starting training 1 out of 3---------"
+        echo "Hello user, to start your first challenge try appending to the same file you echoed."
         echo "Use the command \"cat stage\" to check contents of file."
         echo "*cat will be explained in the next level*"
 	echo "Your new key is 2"
 ;;
 2)
-        echo "Now try to print \" I\'m \$Here \&Today"
+	echo "---------starting training 2 out of 3---------"
+        echo "Now try to print \"I\"m \$Here #Today\""
 	echo "You new key is 3"
 ;;
 3)
+	echo "---------starting training 3 out of 3---------"
         echo -e "Print: Weee \vWeee \vWeee"
+;;
+4)
+	echo "You realize that there are only three challenges right?"
+	echo "Just go to the boss already."
+	echo "sho. sho."
 ;;
 *)
 	echo "Give the right key"
