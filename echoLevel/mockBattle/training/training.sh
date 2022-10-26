8c0c9ba99a3f2399496852c1765c465c13c22a35
Gabriel Lopez <gabriel.andres.lopez03@gmail.com>
2022-10-26 00:24:43 -0400

    added startup and restart directories to be used

--- echoLevel/mockBattle/training/training.sh
+++ echoLevel/mockBattle/training/training.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-echo "Hello this is your training. Echo a statement into a file called \"stage\" and we can get started."
+echo "----------Entering Training----------"
+echo "Hello user, this is your training. Echo a statement into a file called \"stage\" and we can get started."
 echo "Once done run the challenges.sh as you would with a script, but add a space and then a \"1\"."
 echo "This will be your key to enter each challenge"
