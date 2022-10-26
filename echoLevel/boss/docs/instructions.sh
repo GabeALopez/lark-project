e8eff2ad603bf3a59556d285eaa40d8b56f81c50
Gabriel Lopez <gabriel.andres.lopez03@gmail.com>
2022-10-26 00:06:33 -0400

    added to boss folder by having player inject code in script file

---
+++ echoLevel/boss/docs/instructions.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Found hidden directory .startup."
+echo "Corruption code to inject:"
+echo "\"cat ../Desktp/.password\" "
+echo "Run restart script to restore in case of messup"
