9aee2de3c515d43e1c6277929a2a4e8f6b9c8111
GabvinBinder <gavinbinder0433@gmail.com>
2022-11-12 18:07:01 -0500

    Started touch tutorial

--- touchLevel/touchTutorial/mainBase/trainingArea/training.sh
+++ touchLevel/touchTutorial/mainBase/trainingArea/training.sh
@@ -2,8 +2,8 @@
 
 echo "You enter the training area."
 echo "(Trainer): No time to waste. We will begin immediately."
-echo "(Trainer): First, try to echo my name."
-echoName="echo \"Trainer\""
+echo "(Trainer): Now try to touch a new file called newFile by typing: touch newFile"
+echoName="touch newFile"
 counter=0
 while :
 do
@@ -12,33 +12,9 @@ do
 	then
 		break
 	else
-		#echo ${echoName}
-		#echo "$input"
-		((counter=counter+1))
-		if [ $counter == 3 ]
-		then
-			echo "My names on the screen and you still don't know it? I'm losing hope here. My name is Trainer. Now try again."
-			continue
-		fi
 		echo "Try again."
 		continue
 		
 	fi
 done
-echo "(Trainer): Good, now for the last part to this training, try to type this password to the password.sh file: password"
-password="echo \"password\" >> password.sh"
-while :
-do
-	IFS= read -r i
-        if [ "$i" == "$password" ]
-        then
-                break
-        else
-                #echo ${echoName}
-                #echo "$input"
-                echo "Try again."
-                continue
-
-        fi
-done
-echo "(Trainer): Good, now move onto the simulation for more some field experience."
+echo "(Trainer): Good, now move onto the mission"
