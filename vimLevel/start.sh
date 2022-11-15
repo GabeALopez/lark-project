8ea5beaf06a89b42a8dd013b5ee9966e602fe17e
Gabriel Lopez <gabriel.andres.lopez03@gmail.com>
2022-11-14 23:36:25 -0500

    Added a script to start the vim level

---
+++ vimLevel/start.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo "Welcome back. After that attack we had from the enemy
+	it would seem it is now time to take the fight to them.
+	We are equiping you with a prototype of ours. The Virus
+	Immobilization Machine or VIM for short. With this gadget
+	you can can pretty much do anything. But it looks like 
+	we might be just out of the time. We are getting close 
+	to the drop off point. Get in there and have some fun.
+	We will be waiting for on the other side."
+	
+
+
