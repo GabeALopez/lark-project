d50d25fbd324a65bdba5936dd42049005ee7b06b
Gabriel Lopez <gabriel.andres.lopez03@gmail.com>
2022-11-15 19:54:33 -0500

    Added the start of the movment script

---
+++ vimLevel/check.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+input="./entrance.txt"
+count=0
+gateChar="c"
+
+count=`awk -F\g '{total += NF-1} END {print total} ' ./entrance.txt` 
+
+if [ `grep o ./entrance.txt | wc -l` == 1 ]
+then
+	gateChar="o"	
+fi
+ 
+if [ $count == 0 ] && [ "$gateChar" == "o" ]
+then
+echo "Go forward"
+fi
