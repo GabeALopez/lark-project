39e8a8a2e8367432be5b6ee2fe28aa9dd8df13e0
GabvinBinder <gavinbinder0433@gmail.com>
2022-11-19 13:49:50 -0500

    VIM movement tutorial

---
+++ vimLevel/vimTutorial.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo "(Mission Briefer): We are to entrust you to a special mission this time."
+echo "(Mission Briefer): (What you need to do)"
+echo "(Mission Briefer): In order to complete the assignment, you'll need this."
+echo "------------VIM TOOL AQUIRED--------------"
+echo "|     VIM is a multi usage text editor.    |"
+echo "|     Used to edit files, and program      |"
+echo "|     in the bash programming language.    |"
+echo "|     vim into a file by using:            |"
+echo "|           vim (filename)                 |"
+echo "--------------------------------------------"
+echo "(Mission Briefer): Now, we do not have time, so I must leave you on your own for this one. I have put together a file called movementCommands listing all the the basic commands to navigate a file in vim for you. If need be, try practicing on your own there."
