dd242431d9932948db2a74bcf0f63b41820979b1
GabvinBinder <gavinbinder0433@gmail.com>
2022-11-21 16:59:56 -0500

    changes to check while file

--- vimLevel/whileFloor/inputKey.sh
+++ vimLevel/whileFloor/inputKey.sh
@@ -10,9 +10,7 @@ input2="./$input1"
 #Tags
 line1=true
 line4=false
-line5=false
 line6=false
-line7=false
 
 #Check to see if file exists in the first place
 if test -f "$input2"
@@ -31,46 +29,29 @@ then
 #Do what you want above
 #Turns off the first and turns on third tag
       line1=false
-      line3=true
+      line4=true
 #continue use to keep the program from checking
 #every if statement
       continue
     fi
 
 #Check for line
-    if [ "$line" = "while [ $key != $password ]" ] && [ $line3 = true ]
+    if [ "$line" = "while [[ $key != $password ]]" ] && [ $line3 = true ]
     then
       echo "Very good"
-      line3=false
-      line4=true
+      line4=false
+      line6=true
       continue
     fi
 
 #check for line
-    if [ "$line" = "do" ] && [ $line4 = true ]
+    if [ "$line" = "	((key = key+1))" ] && [ $line4 = true ]
     then
       echo "Congrats"
-      line4=false
-      line5=true
+      line6=false
 #converts user defined file to a script file
       exit
     fi
-
-    if [ "$line" = "	$key = $key + 1" ] && [ $line5 = true ]
-    then
-      echo "Very good"
-      line5=false
-      line6=true
-      continue
-    fi
-    if [ "$line" = "done" ] && [ $line6 = true ]
-    then
-      echo "Very good"
-      line6=false
-      line7=true
-      continue
-    fi
-    
   done < $input1
 
 
