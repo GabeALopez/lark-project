a377ced7aba8f501c9203e67163cf21472409864
GabvinBinder <gavinbinder0433@gmail.com>
2022-11-23 14:43:06 -0500

    while/if script for vim boss

--- vimLevel/boss/boss.sh
+++ vimLevel/boss/boss.sh
@@ -2,6 +2,16 @@
 
 #Use while loop to print out
 #numbers from 0-1000
+num=0
+while [[ $num != 1000 ]]
+do
+	if (( $num % 3 == 0 ))
+	then
+		cat $num >> file.txt
+	fi
+done
+
+
 
 #Check if number is divisible by 3
 ##cat numbers to a file
