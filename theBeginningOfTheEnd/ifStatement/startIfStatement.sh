e7d671500ccab450776af4e1d5d45f141c71a11f
Gabriel Lopez <gabriel.andres.lopez03@gmail.com>
2022-11-25 13:09:28 -0500

    Minor text edits to account for validation scripts

--- theBeginningOfTheEnd/ifStatement/startIfStatement.sh
+++ theBeginningOfTheEnd/ifStatement/startIfStatement.sh
@@ -3,4 +3,5 @@
 echo "Alright agent the gate ahead is locked. Damn, they 
 	destroyed the entry script. It looks like we need 
 	to fix the script using VIM. Do it before anymore 
-	guards show up."
+	guards show up. There are some validation scripts
+	to see if your plan was correct."
