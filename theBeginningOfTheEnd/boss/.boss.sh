eb213eec84c8fbea20f2a35cff46fdb5ee35a015
Gabriel Lopez <gabriel.andres.lopez03@gmail.com>
2022-11-25 13:19:50 -0500

    Deleted pieces of .boss.sh script so player can use it

--- theBeginningOfTheEnd/boss/.boss.sh
+++ theBeginningOfTheEnd/boss/.boss.sh
@@ -1,21 +1,10 @@
 #!/bin/bash
 
+#cat bossValidation.sh
 #Use while loop to print out
 #numbers from 0-1000
-num=0
-while [[ $num != 1000 ]]
-do
-	((num=num+1))
-	if (( $num % 3 == 0 ))
-	then
-		echo $num >> file.txt
-	fi
-done
-
 
 
 #Check if number is divisible by 3
 ##echo numbers to a file
 
-#------Another script?---------
-#Other script checks for numbers
