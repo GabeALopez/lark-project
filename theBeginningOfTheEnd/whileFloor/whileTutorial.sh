9e58139dd65738edc883b32134e14e1d2dd0119c
GabvinBinder <gavinbinder0433@gmail.com>
2022-11-25 12:13:31 -0500

    while tutorial

---
+++ theBeginningOfTheEnd/whileFloor/whileTutorial.sh
@@ -0,0 +1,10 @@
+#!bin/bash
+# In vim, you can use a while loop to do a command(s) multiple times so long as a condition is satisified.
+
+# The syntax is as follows:
+# while [[ condition ]]
+# do
+# 	commands
+# done
+
+# While uses same conditonals as the if statement. 
