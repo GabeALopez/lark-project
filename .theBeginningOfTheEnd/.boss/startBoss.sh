467beb35835cb837b5d935c1cedfbde092a58bb6
Gabriel Lopez <gabriel.andres.lopez03@gmail.com>
2022-11-27 10:59:28 -0500

    Hid boss and added story for boss vimLevel

---
+++ .theBeginningOfTheEnd/.boss/startBoss.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+fold -sw 50 <.story.txt
+mv .boss.sh boss.sh
+mv .bossValidation.sh bossValidation.sh
--- .theBeginningOfTheEnd/boss/startBoss.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "Aww, so I see you discovered my master plan.
-	(dialogue dialogue). Nanomachines, son."
-echo "Agent finish him off with your script!"
-mv .boss.sh boss.sh
-mv .bossValidation.sh bossValidation.sh
