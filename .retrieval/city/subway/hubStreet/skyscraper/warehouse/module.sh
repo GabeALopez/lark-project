9436dbefcfd0232162455594d33f523d711c0cc3
Gabriel Lopez <gabriel.andres.lopez03@gmail.com>
2022-11-27 14:15:09 -0500

    Fixed grammer

--- .retrieval/city/subway/hubStreet/skyscraper/warehouse/module.sh
+++ .retrieval/city/subway/hubStreet/skyscraper/warehouse/module.sh
@@ -2,6 +2,6 @@
 echo "Using your master skills, you sneak by the guards and break
 	the lock. You search through the mounds of crates and
 	see a shimer. There it is, the module. You sneak out
-	as fast and quiet when you came in."
+	as fast and quietly when you came in."
 echo "Module Obtained!"
 mv ../../../../../building/.install.sh ../../../../../building/install.sh 
