c29935194b36741868c2bc34d9f22d5b23d71d3e
GabvinBinder <gavinbinder0433@gmail.com>
2022-12-03 18:20:46 -0500

    ending story and ascii

--- .theBeginningOfTheEnd/.finalFloor/.story.sh
+++ .theBeginningOfTheEnd/.finalFloor/.story.sh
@@ -7,6 +7,11 @@ echo "Ha, do you think that will work
 ;;
 2)
 echo "*cough* *cough* So you have defeated me after all *cough*"
+
+fold -s .endingStory.txt
+echo ""
+cat .gameCleared.txt
+echo ""
 echo "  ----------Credits----------
         storyWriter=thomasSpangler
         betaTester+ASCII=alexanderGo
